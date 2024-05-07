2b1a277ec71084b51edfa293a4c0393e71f680da
Ilia Kuzmin <mail@iliakuzmin.tech>
2024-05-07 19:09:06 +0400

    empty buy fragment

---
+++ apps/wallet/instance/src/main/java/com/tonapps/tonkeeper/fragment/trade/buy/BuyFragment.kt
@@ -0,0 +1,11 @@
+package com.tonapps.tonkeeper.fragment.trade.buy
+
+import com.tonapps.tonkeeperx.R
+import uikit.base.BaseFragment
+
+class BuyFragment : BaseFragment(R.layout.fragment_buy) {
+    companion object {
+        fun newInstance() = BuyFragment()
+    }
+
+}
