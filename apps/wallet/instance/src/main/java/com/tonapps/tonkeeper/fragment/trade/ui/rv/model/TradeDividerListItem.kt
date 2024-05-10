2174b6aceb0036941d10a1a8978da40e94c6ea8d
Ilia Kuzmin <mail@iliakuzmin.tech>
2024-05-10 12:52:38 +0400

    trade divider list item

--- apps/wallet/instance/src/main/java/com/tonapps/tonkeeper/fragment/trade/ui/rv/model/TradeDividerListItem.kt
+++ apps/wallet/instance/src/main/java/com/tonapps/tonkeeper/fragment/trade/ui/rv/model/TradeDividerListItem.kt
@@ -1,3 +1,6 @@
 package com.tonapps.tonkeeper.fragment.trade.ui.rv.model
 
-data class TradeDividerListItem()
+import com.tonapps.tonkeeper.fragment.trade.ui.rv.TradeAdapter
+import com.tonapps.uikit.list.BaseListItem
+
+object TradeDividerListItem : BaseListItem(TradeAdapter.TYPE_DIVIDER)
