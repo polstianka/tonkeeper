38ac9c43b5a2cf5c93b9ec2b64980ecb893d0da8
Ilia Kuzmin <mail@iliakuzmin.tech>
2024-05-10 11:00:08 +0400

    implemented basic amount input logic

--- apps/wallet/instance/src/main/java/com/tonapps/tonkeeper/fragment/trade/buy/BuyFragment.kt
+++ apps/wallet/instance/src/main/java/com/tonapps/tonkeeper/fragment/trade/buy/BuyFragment.kt
@@ -1,6 +1,14 @@
 package com.tonapps.tonkeeper.fragment.trade.buy
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.widget.doOnTextChanged
+import com.tonapps.tonkeeper.fragment.send.view.AmountInput
+import com.tonapps.tonkeeper.fragment.trade.buy.vm.BuyViewModel
 import com.tonapps.tonkeeperx.R
+import core.extensions.observeFlow
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import uikit.base.BaseFragment
 
 class BuyFragment : BaseFragment(R.layout.fragment_buy) {
@@ -8,4 +16,17 @@ class BuyFragment : BaseFragment(R.layout.fragment_buy) {
         fun newInstance() = BuyFragment()
     }
 
+    private val viewModel: BuyViewModel by viewModel()
+    private val input: AmountInput?
+        get() = view?.findViewById(R.id.value)
+    private val rateTextView: AppCompatTextView?
+        get() = view?.findViewById(R.id.rate)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        input?.doOnTextChanged { text, _, _, _ ->
+            viewModel.onAmountChanged(text.toString())
+        }
+        observeFlow(viewModel.totalFiat) { rateTextView?.text = it }
+    }
 }
--- apps/wallet/instance/src/main/java/com/tonapps/tonkeeper/fragment/trade/buy/vm/BuyViewModel.kt
+++ apps/wallet/instance/src/main/java/com/tonapps/tonkeeper/fragment/trade/buy/vm/BuyViewModel.kt
@@ -1,4 +1,48 @@
 package com.tonapps.tonkeeper.fragment.trade.buy.vm
 
-class BuyViewModel {
+import androidx.lifecycle.ViewModel
+import com.tonapps.icu.CurrencyFormatter
+import com.tonapps.tonkeeper.fragment.trade.domain.GetRateFlowCase
+import com.tonapps.wallet.data.core.WalletCurrency
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+
+class BuyViewModel(
+    getRateFlowCase: GetRateFlowCase
+) : ViewModel() {
+
+    companion object {
+        private const val TOKEN_TON = "TON"
+    }
+    // todo
+    private val currency = MutableStateFlow(WalletCurrency.DEFAULT)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val rate = currency.flatMapLatest { getRateFlowCase.execute(it) }
+        .map { it.rate(TOKEN_TON) }
+        .filterNotNull()
+        .map { it.value }
+
+    private val amount = MutableStateFlow(0f)
+
+    val totalFiat = combine(amount, rate, currency) { amount, rate, currency ->
+        val totalAmount = amount * rate
+        CurrencyFormatter.format(currency.code, totalAmount)
+    }
+
+    fun onAmountChanged(amount: String) {
+        val oldAmount = this.amount.value
+        val newAmount = amount.getValue()
+        if (oldAmount == newAmount) return
+
+        this.amount.value = newAmount
+    }
+
+    private fun String.getValue(): Float {
+        return toFloatOrNull() ?: 0f
+    }
 }
