2348842c1a47423e248c967d5076a6aa9ee8890f
BarbedRuff <Barbed.Ruff@yandex.ru>
2024-05-23 16:25:31 +0300

    Slippage(feat): Created slippage screen

--- apps/wallet/instance/src/main/java/com/tonapps/tonkeeper/ui/screen/swap/SwapScreen.kt
+++ apps/wallet/instance/src/main/java/com/tonapps/tonkeeper/ui/screen/swap/SwapScreen.kt
@@ -2,37 +2,49 @@ package com.tonapps.tonkeeper.ui.screen.swap
 
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.tonapps.tonkeeper.sign.SignRequestEntity
 import com.tonapps.tonkeeper.ui.screen.root.RootViewModel
+import com.tonapps.tonkeeper.ui.screen.settings.slippage.SlippageScreen
 import com.tonapps.tonkeeperx.BuildConfig
 import com.tonapps.tonkeeperx.R
 import org.koin.androidx.viewmodel.ext.android.activityViewModel
 import uikit.base.BaseFragment
 import uikit.extensions.applyNavBottomPadding
 import uikit.extensions.getDimensionPixelSize
-import uikit.widget.webview.bridge.BridgeWebView
+import uikit.navigation.Navigation
+import uikit.widget.HeaderView
 
-class SwapScreen: BaseFragment(R.layout.fragment_swap), BaseFragment.BottomSheet {
+class SwapScreen: BaseFragment(R.layout.fragment_swap), BaseFragment.BottomSheet
+{
 
     private val args: SwapArgs by lazy { SwapArgs(requireArguments()) }
 
     private val rootViewModel: RootViewModel by activityViewModel()
 
-    private lateinit var webView: BridgeWebView
+    private lateinit var headerView: HeaderView
+    private var navigation: Navigation? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        webView = view.findViewById(R.id.web)
-        webView.clipToPadding = false
-        webView.applyNavBottomPadding(requireContext().getDimensionPixelSize(uikit.R.dimen.offsetMedium))
-        webView.loadUrl(getUri().toString())
-        webView.jsBridge = StonfiBridge2(
-            address = args.address,
-            close = ::finish,
-            sendTransaction = ::sing
-        )
+        headerView = view.findViewById(R.id.header)
+        navigation = Navigation.from(view.context)
+        headerView.doOnCloseClick = {
+            navigation?.add(SlippageScreen.newInstance())
+        }
+        headerView.doOnActionClick = { this.finish() }
+        headerView.clipToPadding = false
+        headerView.applyNavBottomPadding(requireContext().getDimensionPixelSize(uikit.R.dimen.offsetExtraExtraSmall))
+
+//        webView = view.findViewById(R.id.web)
+//        webView.clipToPadding = false
+//        webView.applyNavBottomPadding(requireContext().getDimensionPixelSize(uikit.R.dimen.offsetMedium))
+//        webView.loadUrl(getUri().toString())
+//        webView.jsBridge = StonfiBridge2(
+//            address = args.address,
+//            close = ::finish,
+//            sendTransaction = ::sing
+//        )
     }
 
     private fun getUri(): Uri {
@@ -49,11 +61,6 @@ class SwapScreen: BaseFragment(R.layout.fragment_swap), BaseFragment.BottomSheet
         request: SignRequestEntity
     ): String {
         return rootViewModel.requestSign(requireContext(), request)
-    }
-
-    override fun onDestroyView() {
-        webView.destroy()
-        super.onDestroyView()
     }
 
     companion object {
